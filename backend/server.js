139f307c5b62b7624b86beb368a961561805d283
I585235 <garvit.jindal@sap.com>
2025-05-10 17:01:43 +0530

    loggin env name

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 const envFile = `.env.${process.env.APP_ENV || 'development'}`;
 dotenv.config({ path: envFile });
 const connectDB = require('./config/db');
-
+console.log(process.env.APP_ENV);
 const PORT = process.env.PORT;
 connectDB();
 
